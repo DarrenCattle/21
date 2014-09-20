66e69e90206fb8b38d0a6e64a445eb7f05980307
Alejo Rivera <alejoriveralara@gmail.com>
2014-09-20 17:20:05 -0600

    migration days add default 0

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920215741) do
+ActiveRecord::Schema.define(version: 20140920230942) do
 
   create_table "challenges", force: true do |t|
     t.integer  "days"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140920215741) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.integer  "day",        default: 0
   end
 
   create_table "users", force: true do |t|
