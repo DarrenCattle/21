6308ac9893410b5635a36bdaeda0088ea5e3b5ff
Max Holzheu <maxhm10@gmail.com>
2014-09-20 16:49:20 -0600

    add name to challenges, my-challenge partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
   	if user_signed_in?
   		@current_challenge = current_user.current_challenge
+  		@challenge = current_user.challenges.build
   	end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920203452) do
+ActiveRecord::Schema.define(version: 20140920215741) do
 
   create_table "challenges", force: true do |t|
     t.integer  "days"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140920203452) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "users", force: true do |t|
