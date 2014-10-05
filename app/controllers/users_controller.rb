57744f72420146feba2fff9fed441e0944ccb136
Alejo Rivera <alejoriveralara@gmail.com>
2014-10-04 22:23:00 -0600

    fill in profile page with current challenge and feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
+  	@activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user)
   end
 end
