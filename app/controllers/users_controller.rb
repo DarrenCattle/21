8488009a59f8bb599d24b00a072bdad2677f9a7f
Alejo Rivera <alejoriveralara@gmail.com>
2014-10-04 23:45:55 -0600

    add before_filter authenticate user to challenges and users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_filter :authenticate_user!
+
   def show
   	@user = User.find(params[:id])
   	@activities = PublicActivity::Activity.order("created_at desc").where(owner_id: @user)
