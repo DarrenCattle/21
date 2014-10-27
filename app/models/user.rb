bfdc62ff7862ffee21f8747480274f8ca7f0777e
Alejo Rivera <alejoriveralara@gmail.com>
2014-10-26 21:09:28 -0600

    fix user model undefined method problem when user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
   has_many :challenges, dependent: :destroy
 
   def current_challenge
-  	if challenges.last.day < 21
-  		challenges.last
-  	else
+  	if challenges.last.blank? or challenges.last.day > 21
   		nil
+  	else
+  		challenges.last
   	end
   end
 end
