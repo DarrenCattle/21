7de4d62dabce3ed5d728ffbd27e3fcf3a9791bf8
Alejo Rivera <alejoriveralara@gmail.com>
2014-10-27 22:49:07 -0600

    add pagination for user profiles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
   		@current_challenge = current_user.current_challenge
   		@challenge = current_user.challenges.build
 
-      @activities = PublicActivity::Activity.paginate( page: params[:page], per_page: 25 ).order(created_at: :desc)
+      @activities = PublicActivity::Activity.order(created_at: :desc).paginate( page: params[:page], per_page: 25 )
 
   	end
   end
