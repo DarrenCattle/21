8d6de23104e8fb689304f67bc65aa83b7a283cfe
Alejo Rivera <alejoriveralara@gmail.com>
2014-10-30 10:23:12 -0600

    change day position in feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
   		@current_challenge = current_user.current_challenge
   		@challenge = current_user.challenges.build
 
-      @activities = PublicActivity::Activity.order(created_at: :desc).paginate( page: params[:page], per_page: 25 )
+      @activities = PublicActivity::Activity.order(created_at: :desc).paginate( page: params[:page], per_page: 15 )
 
   	end
   end
