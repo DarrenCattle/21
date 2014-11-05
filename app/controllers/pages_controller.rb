f20ef9b3a26edc7da123c69db2b8cb5256229fb1
Max Holzheu <maxhm10@gmail.com>
2014-11-04 23:34:45 -0600

    add comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,17 +9,7 @@ class PagesController < ApplicationController
 
       @activities = Activity.order(created_at: :desc).paginate( page: params[:page], per_page: 15 )
 
-      commentable = @activities.last
-
-      puts "****** I'm here *********"
-      comment = commentable.comments.create
-      comment.title = "First comment."
-      comment.comment = "This is the first comment."
-      comment.save!
-      puts "****** I'm there *********"
-      puts "****** #{comment.reload.inspect} *********"
-
-      @comments = commentable.comments.recent.limit(10).all
+      @comment = Comment.new
   	end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   put 'challenges/reset_entered', as: :reset_entered
   resources :challenges
 
+  resources :comments, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
