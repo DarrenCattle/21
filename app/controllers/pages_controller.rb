47de8cf4a881c06eb9fa4e01d62307697a88c32c
Max Holzheu <maxhm10@gmail.com>
2014-11-05 22:41:35 -0600

    improve comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,10 @@ class PagesController < ApplicationController
     @no_navbar  = true
     
   	if user_signed_in?
-
   		@current_challenge = current_user.current_challenge
   		@challenge = current_user.challenges.build
 
       @activities = Activity.order(created_at: :desc).paginate( page: params[:page], per_page: 15 )
-
       @comment = Comment.new
   	end
   end
@@ -18,6 +16,5 @@ class PagesController < ApplicationController
 
   def contact
   end
-
-    
+  
 end
