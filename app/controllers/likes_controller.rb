d58e3ccf568173c96ab3d9bacea63ed150054897
Max Holzheu <maxhm10@gmail.com>
2014-11-06 02:12:54 -0600

    add likes. liking works, unliking needs some help

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,13 @@ class LikesController < ApplicationController
 	before_action :authenticate_user!
 
   def create
-  	@activity = Activity.find(params[:id])
+  	@activity = Activity.find(params[:like][:activity_id])
   	current_user.like!(@activity)
   end
 
+  # This doesn't quite work
   def destroy
-  	@activity = Activity.find(params[:id])
+  	@activity = Activity.find(params[:like][:activity_id])
   	current_user.unlike!(@activity)
   end
 end
